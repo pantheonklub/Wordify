6eb0abbeafc3354e527bbb6f079c45bfcdfc794a
Vinay Rajan S <vinayrajan04@gmail.com>
2024-07-11 12:56:17 +0530

    .

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 
+const butt = document.querySelectorAll('button');
+// butt.addEventListener("click", generate(butt.id))
+console.log(butt)
 
-function related(){
+function generate(param){
     let value = document.getElementById('value').value;
-    let url = `https://api.datamuse.com/words?rel_trg=${value}`;
+    let url = `https://api.datamuse.com/words?${param}=${value}`;
     let str = []
     
     fetch(url)
-        .then(response => response.json())
-        .then(data => {
+    .then(response => response.json())
+    .then(data => {
             if (data.length != 0){
                 for (i in data){
                    str.push((data[i]['word']));
@@ -18,73 +21,12 @@ function related(){
             }
             
         });
-        
-        setTimeout(() => {
-            
+
+        setTimeout(() =>{
+
             vals = str.slice(0,70).toString();
-            
             document.getElementById('result').innerHTML = vals;
-            
-        },10);
+        },100)
+
     } 
-    
-    function rhymes(){
-        let value = document.getElementById('value').value;
-        let url = `https://api.datamuse.com/words?rel_rhy=${value}`;
-        let str = []
-        
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.length != 0){
-                for (i in data){
-                    str.push((data[i]['word']));
-                }
-            }
-            
-            else{
-                str.push('404 Word Not Found! :(')
-            }
-        });
-        
-        setTimeout(() => {
-            
-            vals = str.slice(0,70).toString()
-            
-            document.getElementById('result').innerHTML = vals;
-            
-        },10);
-    } 
-    
-    
-    
-    
-    
-    function starend(){
-        let value = document.getElementById('value').value;
-        let url = `https://api.datamuse.com/words?sp=${value}`;
-        let str = []
-        
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.length != 0){
-                for (i in data){
-                    str.push((data[i]['word'])+'');
-                }
-            }
-            
-            else{
-                str.push('404 Word Not Found! :(')
-            }
-        });
-        
-        
-    setTimeout(() => {
-        
-        vals = str.slice(0,70).toString()
-    
-        document.getElementById('result').innerHTML = vals;
-    
-    },10);
-} 
+      
